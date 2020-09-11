7591811cafa7254618b82d1b2408128131e401a7
Chandrakanth Rajesh <54387130+chandrakanthrajesh@users.noreply.github.com>
2020-09-11 16:13:19 -0400

    no message

--- ck.js
+++ ck.js
@@ -1,1 +1,11 @@
 console.log("Hello World!")
+
+var http = require('http');
+var server = http.createServer(function(req,res)
+{
+    res.writeHead(200,
+        {"Content-Type":"text/plain"});
+
+});
+
+server.listen(3000);
